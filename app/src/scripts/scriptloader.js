3044912c96873471cbb90ca07c0e6098b66cf26d
serraventura <serraventura@gmail.com>
2015-05-26 23:15:36 +0100

    util functions

--- app/src/scripts/scriptloader.js
+++ app/src/scripts/scriptloader.js
@@ -20,6 +20,10 @@ myFanPageApp1.scriptloader = (function(){
 			css.setAttribute('href', file);
 			document.body.appendChild(css);
 
+		},
+		replaceURLWithHTMLLinks: function(text) {
+			var exp = /(\b(https?|ftp|file):\/\/([-A-Z0-9+&@#%?=~_|!:,.;]*)([-A-Z0-9+&@#%?\/=~_|!:,.;]*)[-A-Z0-9+&@#\/%=~_|])/ig;
+			return text.replace(exp, "<a href='$1' target='_blank'>$3</a>");
 		}
 
 	}
