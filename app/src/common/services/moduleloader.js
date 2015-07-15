398ab621e5a0206825cf5dbdc3e91a479fc83854
serraventura <serraventura@gmail.com>
2015-07-15 22:38:00 +0100

    plugin support development

--- app/src/common/services/moduleloader.js
+++ app/src/common/services/moduleloader.js
@@ -11,41 +11,84 @@ angular.module('myFanPageApp')
             if (FanPageConfig.hasOwnProperty('plugin')) {
 
                 var p;
+                var arrDirectiveElements;
                 var arrPlugins = [];
                 var scope = $rootScope.$new();
 
                 for(p in FanPageConfig.plugin){
 
                     if (FanPageConfig.plugin.hasOwnProperty(p) && FanPageConfig.plugin[p]) {
+
+                      //TODO: files should be loaded by LazyLoad module not by "hand"
+
                         MYFP.util.loadJS('src/webcomponent/'+p.toLowerCase()+'/'+p.toLowerCase()+'.js');
                         arrPlugins.push('src/webcomponent/'+p.toLowerCase()+'/'+p.toLowerCase()+'.js');
 
-                        $http.get('src/webcomponent/'+p.toLowerCase()+'/'+p.toLowerCase()+'.js').then(function (a,b,c) {
-                            console.log(a)
-                        });
+                        $http.get('src/webcomponent/'+p.toLowerCase()+'/'+p.toLowerCase()+'.js').then(function (fileContent) {
 
-                        $ocLazyLoad.load({
-                            name: p.toLowerCase(), 
+                          var regExp = /\.directive\(['"]([^'"\n]+)['"]/g;
+                          var match = fileContent.data.match(regExp);
+
+                          var arrDirectives = match.toString()
+                            .replace(new RegExp('.directive\\(', 'g'), '')
+                            .replace(new RegExp("\\'", 'g'), '')
+                            .replace(new RegExp('\\"', 'g'), '').split(',');
+
+                          arrDirectiveElements = [];
+
+                          for (var i = 0; i < arrDirectives.length; i++) {
+
+                            arrDirectiveElements.push(
+                              ('<' + MYFP.util.camelCaseToDash(arrDirectives[i]) + '></' + MYFP.util.camelCaseToDash(arrDirectives[i]) + '>').toLowerCase()
+                            );
+
+                          }
+
+                          //TODO: the whole logic to check plugins directives should happen after lazyload
+                          $ocLazyLoad.load({
+                            name: p.toLowerCase(),
                             files: ['src/webcomponent/'+p.toLowerCase()+'/'+p.toLowerCase()+'.js'],
                             serie: true
-                        }).then(function () {
+                          }).then(function () {
 
                             var el, elToAppend;
-                            elToAppend = $compile('<my-test-plugin></my-test-plugin>')(scope);
-                            el = angular.element('body').find('my-test-plugin').parent();
-                            angular.element('body').find('my-test-plugin').remove();
-                            el.append(elToAppend);
 
-                        }, function (e) {
+                            for (var i = 0; i < arrDirectiveElements.length; i++) {
+
+                              //TODO: get attributes from directives found in the DOM
+                              //TODO: remove duplicated directives
+                              $('body').find(
+                                arrDirectiveElements[i].split('<')[1].replace('>', '')
+                              ).each(function(){
+                                  
+                                  $(this).parent().append(elToAppend); //TODO: get attributes
+
+                                  //TODO: get and apply properties before compiling
+                                  elToAppend = $compile( angular.element( arrDirectiveElements[i] ) )(scope);
+
+                              });
+
+                              //el = angular.element('body').find('my-test-plugin').parent();
+                              //angular.element('body').find('my-test-plugin').remove();
+                              //el.append(elToAppend);
+
+                            }
+
+                          }, function (e) {
                             console.log(e);
+                          });
+
+
                         });
+
+
 
                     };
 
                 };
 
                 // return $ocLazyLoad.load({
-                //     name: '', 
+                //     name: '',
                 //     files: arrPlugins
                 // });
 
