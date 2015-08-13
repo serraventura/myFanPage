fd6bd96b16f6efd19e1d5518b9a08410ebcd9ea9
Thiago Ventura <serraventura@gmail.com>
2015-08-13 17:08:14 +0100

    coding cache handler

--- app/src/core/controllers/webcontent.js
+++ app/src/core/controllers/webcontent.js
@@ -10,10 +10,9 @@ angular.module('myFanPageApp')
     PageDetailService,
     PhotoService,
     FeedService,
-    ModuleLoader
+    ModuleLoader,
+    FanPageContent
   ) {
-
-
 
     var arrPromises = [];
     var promise;
@@ -47,7 +46,48 @@ angular.module('myFanPageApp')
     });
     arrPromises.push(promise);
 
+    function makeCache() {
+      
+        var fanPageContentCache = {
+          dateCriation: moment().format(),
+          data: FanPageContent
+        };
+
+        try{
+          fanPageContentCache = JSON.stringify(fanPageContentCache);
+        }catch(err){
+          console.log('Error generating cache: ', err);
+        }
+
+        localStorage.setItem('fanPageContentCache', fanPageContentCache);
+
+    };
+
     $q.all(arrPromises).then(function (data) {
+
+      // handling cache
+      if(typeof Storage !== 'undefined'){
+
+        if(localStorage.getItem('fanPageContentCache') != null){
+
+          try{
+
+            var cache = JSON.parse(localStorage.getItem('fanPageContentCache'));
+
+            if (moment().diff(moment(cache.dateCriation), 'days') >= 1) {
+              localStorage.removeItem('fanPageContentCache');
+              makeCache();
+            };
+
+          }catch(err){
+            console.log('Error in cache: ', err);
+          }
+
+        }else{
+          makeCache();
+        }
+
+      }
 
     }, function(err){
 
@@ -59,7 +99,5 @@ angular.module('myFanPageApp')
     //   {name: 'test 222', age: 44},
     //   {name: 'test 333', age: 55}
     // ]
-
-
 
   });
