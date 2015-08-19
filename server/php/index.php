4fefddf8f5b0d7ebe2834a2d8695a93c4dd02621
serraventura <serraventura@gmail.com>
2015-08-19 23:00:02 +0100

    PHP version API done.

--- server/php/index.php
+++ server/php/index.php
@@ -1,5 +1,9 @@
 <?php
 
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST');
+header("Access-Control-Allow-Headers: X-Requested-With");
+
 require 'Slim/Slim.php';
 \Slim\Slim::registerAutoloader();
 
@@ -10,10 +14,10 @@ $app = new \Slim\Slim();
 
 $app->get('/identifier/', function() use ($app) {
 
+	$req = $app->request();
 	$TOKEN = '390919341102414|M7umyjZFedSGfPhQ4QXnOvhMXX4';
 	$API = 'https://graph.facebook.com';
-
-	$req = $app->request();
+	$GETPARAMS = $req->params();
 	
 	$id = $req->params('identifier');
 	$endPoint = $req->params('endPoint');
@@ -22,52 +26,38 @@ $app->get('/identifier/', function() use ($app) {
 	$params = '';
 
 	if(isset( $id )){
-		$sufix += '/'+$id;
+		$sufix = $sufix . '/' . $id;
 	}
 
 	if(isset($endPoint) && $endPoint != 'identifier'){
-		$sufix += '/'+$endPoint;
+		$sufix = $sufix . '/' . $endPoint;
 	}
 
-	// unset($req->params('identifier'));
-	// unset($req->params('endPoint'));
+	unset($GETPARAMS['identifier']);
+	unset($GETPARAMS['endPoint']);
 
-	$params += '?access_token=' + $TOKEN + '&';
+	$params = $params . '?access_token=' . $TOKEN . '&';
 
-
-
-var_dump($URL);
-// var_dump($req->params('endPoint'));
- // echo $req->get('endPoint');
-
-
-
-	foreach ($req->params() as $key => $value) {
-		$params += $value + '=' + $key[$value] + '&';
+	foreach ($GETPARAMS as $key => $value) {
+		$params = $params . $key . '=' . $value . '&';
 	};
 
 	$params = substr($params, 0, strlen($params)-1);
 
-	$URL = $API + $sufix + $params;
+	$URL = $API . $sufix . $params;
 
-
-return;
-
-
-
-	$opts = array(
-		'http' => array(
-			'method' => 'GET'
-		)
-	);
-
-	$context = stream_context_create($opts);
-	$res = file_get_contents($URL, false, $context);
+	$curl_handle = curl_init();
+	curl_setopt($curl_handle, CURLOPT_URL,$URL);
+	curl_setopt($curl_handle, CURLOPT_CONNECTTIMEOUT, 2);
+	curl_setopt($curl_handle, CURLOPT_RETURNTRANSFER, 1);
+	curl_setopt($curl_handle, CURLOPT_USERAGENT, 'Your application name');
+	$res = curl_exec($curl_handle);
+	curl_close($curl_handle);
 
 	$response = $app->response();
 	$response['Content-Type'] = 'application/json';
 	$response->status(200);
-	$response->body(json_encode( $res ));
+	$response->body( $res );
 
 });
 
